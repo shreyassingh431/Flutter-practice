a7ab38c38bc1929985bc6debace01d3767537849
shreyassingh431 <30365246+shreyassingh431@users.noreply.github.com>
2019-11-15 09:06:32 +0530

    declaring methods on onpressed in different way.

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,43 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  void answerQuestions() {
+    print('What is Your favourite colour');
+  }
+
+
   @override
   Widget build(BuildContext context) {
+
+    var questArray =[
+      'what is fav colour',
+      'what is fav animal'
+    ];
+
+
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Jḁi Mata di'),
-          ),
-          //body: Text('har har mahadev̥'),
-          body: Column(
-              children: <Widget>[Text('The Question̥'),
-          RaisedButton(child: Text('Answer 1'),
-            onPressed: null,),
-          RaisedButton(child: Text('Answer 2'),
-            onPressed: null,),
-          RaisedButton(child: Text('Answer 3'),
-      onPressed: null,)],
-    )
-    ,
-    )
-    ,
+        appBar: AppBar(
+          title: Text('Jḁi Mata di'),
+        ),
+        //body: Text('har har mahadev̥'),
+        body: Column(
+          children: <Widget>[Text('The Question̥'),
+            RaisedButton(child: Text(questArray[0]),
+              onPressed: answerQuestions,),
+            RaisedButton(child: Text('Answer 2'),
+              onPressed: () => print('3rd button clicked'),),
+            RaisedButton(child: Text('Answer 3'),
+              onPressed: () {
+                /*this type of functions cant be named */
+                /*your stuffs*/
+                print('3rd button clicked');
+              },)
+          ],
+        )
+        ,
+      )
+      ,
     );
   }
 }
